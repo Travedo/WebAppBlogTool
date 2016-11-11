a13a154e383fa48e235512eaa157f5106c689561
Dorette <s0541685@htw-berlin.de>
2016-11-11 09:35:59 +0100

    first push to the github repo, first prototype in progress

---
+++ firstPrototype/BlogTool.js
@@ -0,0 +1,31 @@
+/*$(document).ready(function() {
+	
+	$('.menu-trigger').click(function() {
+		
+		$(".navigation-menu").fadeToggle();
+		
+	});
+	
+	$(window).resize(function() {
+		$(".navigation-menu").removeAttr('style');
+	});
+
+});
+*/
+
+$(document).ready(function() {
+	
+	$('.login-btn').click(function() {
+		
+		$('.login').css({'display': 'block'});
+		/*$(".login").fadeToggle();*/
+		
+	});
+	
+	/*
+	$(window).resize(function() {
+		$(".login-close-btn").removeAttr('style');
+	});
+	*/
+
+});
