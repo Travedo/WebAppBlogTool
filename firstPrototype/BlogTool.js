1c41e009599a5a37f3ee86323aeed22918a2d806
Dorette <s0541685@htw-berlin.de>
2016-11-23 21:08:55 +0100

    responsive machen

--- firstPrototype/BlogTool.js
+++ firstPrototype/BlogTool.js
@@ -1,31 +1,45 @@
-/*$(document).ready(function() {
+/*
+$(document).ready(function() {
 	
 	$('.menu-trigger').click(function() {
 		
-		$(".navigation-menu").fadeToggle();
-		
+		$(".menu").fadeToggle();
 	});
 	
 	$(window).resize(function() {
-		$(".navigation-menu").removeAttr('style');
+
+		$(".menu").removeAttr('style');
 	});
 
 });
-*/
+
 
 $(document).ready(function() {
 	
 	$('.login-btn').click(function() {
 		
 		$('.login').css({'display': 'block'});
-		/*$(".login").fadeToggle();*/
+		$(".login").fadeToggle();
 		
 	});
 	
-	/*
+	
 	$(window).resize(function() {
 		$(".login-close-btn").removeAttr('style');
 	});
-	*/
+	
+
+});
+
+*/
+$(document).ready(function() {
+
+  $('.menu-trigger').click(function() {
+    $(".menu").fadeToggle();    
+  });
+  
+  $(window).resize(function() {
+    $(".menu").removeAttr('style');
+  });
 
 });
