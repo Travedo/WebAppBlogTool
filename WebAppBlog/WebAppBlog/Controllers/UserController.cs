b7bf41bf7d5f97ac141345fb81be487a1b501753
Aijana Schumann <aijana.schumann@highstreet-technologies.com>
2016-11-23 15:27:24 +0100

    initial commit, login, regestir, manage

---
+++ WebAppBlog/WebAppBlog/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebAppBlog.Models;
+
+namespace WebAppBlog.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationUserManager _userManager;
+
+        public UserController() {
+        }
+
+        public UserController(ApplicationUserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: User
+        public async Task<ActionResult> Index()
+        {
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            var model = new UserViewModel {
+                FirstName=user.FirstName,
+                LastName=user.LastName,
+                Birthdate=user.Birthdate,
+                Email=user.Email
+            };
+
+            return View(model);
+        }
+    }
+}
