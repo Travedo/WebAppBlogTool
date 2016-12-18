317a3561146a4d3972d478f85465f290691d32f5
Aijana Schumann <aijana.schumann@gmail.com>
2016-12-18 13:59:41 +0100

    skeleton of working webapi to acceppt userdata for blog creation

---
+++ WebAppBlog/WebAppBlog/Controllers/BlogApiController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAppBlog.Controllers
+{
+    [RoutePrefix("api/BlogApi")]
+    public class BlogApiController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        [Route("CreateBlog")]
+        public HttpResponseMessage CreateBlog(HttpRequestMessage request)
+        {
+            var data = request.Content.ReadAsStringAsync().Result;
+            if (data == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("Post data is empty")
+                };
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+
+
+
+        }
+
+        
+       /* [HttpPost]
+        [Route("CreateBlog")]
+        public string CreateBlog(string value)
+        {
+            Debug.WriteLine(value);
+            return "test";
+        }*/
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- WebAppBlog/WebAppBlog/Global.asax.cs
+++ WebAppBlog/WebAppBlog/Global.asax.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebAppBlog.App_Start;
 
 namespace WebAppBlog
 {
@@ -14,6 +16,7 @@ namespace WebAppBlog
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
