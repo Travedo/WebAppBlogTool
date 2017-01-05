fbf3289d2ebd43cd109cc40daf054f847555d226
Aijana Schumann <aijana.schumann@gmail.com>
2017-01-05 13:47:13 +0100

    started with sending more data needed for blog creation

--- WebAppBlog/WebAppBlog/Scripts/objectCreation.js
+++ WebAppBlog/WebAppBlog/Scripts/objectCreation.js
@@ -18,7 +18,13 @@ function sendData(url, blog){
         dataType: "json",
         complete: function (data) {
             if (data.readyState === 4 & data.status === 200) {
-                window.location.replace(url);
+
+                //workaround to have the current page (create page) be added to browser history
+                setTimeout(function () {
+                    window.location.href=url;
+                }, 0)
+
+                
             }
         }
     });
@@ -29,6 +35,7 @@ function sendData(url, blog){
 function create()
 {
     var blogdata = document.getElementById("blogdata"); // get blog data
+    var blogdatacontinued = document.getElementById("blogdata-continued"); // get blog data in drop area
     var blog = {};
     blog.titel = "";
     blog.subtitel = "";
@@ -38,16 +45,29 @@ function create()
 
     var positionCounter = 0;
     for (var element of blogdata.children) {
+        extratElements(element, blog, positionCounter);
+    }
+    
+    for(var element of blogdatacontinued.children)
+    {
+        extratElements(element, blog, positionCounter);
+    }
+    console.log(blog);
 
-        switch (element.localName) {
-            case "input": //go trough children and add accordingly
+    
+    return blog;
+   
+}
 
-                if (element.id === "blog-titel")
-                {
-                    blog.titel = element.value;
-                } else
-                    if (element.id === "blog-subtitle") {
-                        blog.subtitel = element.value;
+function extratElements(element, blog, positionCounter) {
+    switch (element.localName) {
+        case "input": //go trough children and add accordingly
+            //TODO: handle file input (single) correctly
+            if (element.id === "blog-titel") {
+                blog.titel = element.value;
+            } else
+                if (element.id === "blog-subtitle") {
+                    blog.subtitel = element.value;
                 } else {
 
                     var text = {};
@@ -56,22 +76,28 @@ function create()
                     blog.text.push(text);
                     positionCounter++;
                 }
-               
-                break;
-            case "textarea":
 
-                var text = {};
-                text.value = element.value;
-                text.position = positionCounter;
-                blog.text.push(text);
+            break;
+        case "textarea":
 
-                positionCounter++;
-                break;
-        }
+            var text = {};
+            text.value = element.value;
+            text.position = positionCounter;
+            blog.text.push(text);
+
+            positionCounter++;
+            break;
+
+            //TODO: handle video correctly
+        case "iframe":
+            var text = {};
+            text.value = element.src;
+            text.position = positionCounter;
+            blog.text.push(text);
+
+            positionCounter++;
+            break;
+
+            //TODO: handle gallery correctly
     }
-    console.log(blog);
-
-    
-    return blog;
-   
 }
