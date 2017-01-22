a81cc1d5c773646d52750e65c2f635581c92f30f
Dorette <s0541685@htw-berlin.de>
2017-01-22 22:57:25 +0100

    gallery styles

--- WebAppBlog/WebAppBlog/App_Start/BundleConfig.cs
+++ WebAppBlog/WebAppBlog/App_Start/BundleConfig.cs
@@ -19,8 +19,6 @@ namespace WebAppBlog
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-          
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js",
