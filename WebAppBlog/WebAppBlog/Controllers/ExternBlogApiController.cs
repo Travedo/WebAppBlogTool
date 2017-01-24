4f6cf97d35dfe7bffc58c82e03f60f23d958279b
Aijana Schumann <aijana.schumann@gmail.com>
2017-01-24 01:45:01 +0100

    skeleton of externblogapicontroller

--- WebAppBlog/WebAppBlog/Controllers/ExternBlogApiController.cs
+++ WebAppBlog/WebAppBlog/Controllers/ExternBlogApiController.cs
@@ -12,8 +12,56 @@ namespace WebAppBlog.Controllers
     {
 
         [HttpGet]
-        [Route("Delete")]
-        public HttpResponseMessage Delete(int id)
+        [Route("DeleteBlog")]
+        public HttpResponseMessage DeleteBlog(int id)
+        {
+
+            return null;
+        }
+
+        [HttpPost]
+        [Route("DeleteText")]
+        public HttpResponseMessage DeleteText(HttpRequestMessage request)
+        {
+
+            return null;
+        }
+
+
+        [HttpPost]
+        [Route("DeleteImage")]
+        public HttpResponseMessage DeleteImage(HttpRequestMessage request)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [Route("DeleteGallery")]
+        public HttpResponseMessage DeleteGallery(HttpRequestMessage request)
+        {
+
+            return null;
+        }
+
+        [HttpPost]
+        [Route("AddGallery")]
+        public HttpResponseMessage AddGallery(HttpRequestMessage request)
+        {
+
+            return null;
+        }
+
+        [HttpPost]
+        [Route("AddImage")]
+        public HttpResponseMessage AddImage(HttpRequestMessage request)
+        {
+
+            return null;
+        }
+
+        [HttpPost]
+        [Route("AddText")]
+        public HttpResponseMessage AddText(HttpRequestMessage request)
         {
 
             return null;
