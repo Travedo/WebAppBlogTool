59fc90bfc78cadbf5d1ee0aa2d6078ec3b4a5d9c
Dorette <s0541685@htw-berlin.de>
2017-01-31 19:00:15 +0100

    fix header, integrate homepage.css, remove background image, start using response.min.js

--- WebAppBlog/WebAppBlog/Scripts/homepage.js
+++ WebAppBlog/WebAppBlog/Scripts/homepage.js
@@ -25,3 +25,12 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+
+/* Responsive Images */
+Response.create({
+    prop: "width"  // "width" "device-width" "height" "device-height" or "device-pixel-ratio"
+  , prefix: "min-width- r src"  // the prefix(es) for your data attributes (aliases are optional)
+  , breakpoints: [1000, 760, 320, 0] // min breakpoints (defaults for width/device-width)
+  , lazy: true // optional param - data attr contents lazyload rather than whole page at once
+});
